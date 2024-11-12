6e2e84316f4517842321c25b8a4c21cd84c8e491
Erfan Taheryan <erfantaheryan@gmail.com>
2024-11-12 12:01:44 +0330

    create houses migration

---
+++ app/Database/Migrations/2024-11-12-082831_CreateHouses.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateHouses extends Migration
+{
+    public function up()
+    {
+        // Creating 'houses' table
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'unsigned'      => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'description' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'price' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'bedrooms' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'bathrooms' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'size' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+                'null'       => true, // This can be null if size is not provided
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'default' => 'CURRENT_TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        // Add primary key
+        $this->forge->addPrimaryKey('id');
+
+        // Create the table
+        $this->forge->createTable('houses');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('houses');
+    }
+}
